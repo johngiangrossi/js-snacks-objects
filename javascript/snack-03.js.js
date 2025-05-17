236034ce7381b3ce77ab22837b46bffebc07ad9d
johngiangrossi <john.giangrossi@email.com>
2025-05-17 17:20:30 +0200

    ADD snakc3

--- javascript/snack-03.js.js
+++ javascript/snack-03.js.js
@@ -12,3 +12,24 @@ Crea un array di oggetti che rappresentano degli animali.
 Crea un nuovo array con la lista dei mammiferi
 */
 
+const animali = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+];   //array(object)
+
+console.log(animali);
+const animaliMammiferi = [];  //array(object)
+
+
+for (let i = 0; i < animali.length; i++) {
+    const animale = animali[i];   //array(object)
+    // console.log(i, animale);  
+
+    if (animale.classe === 'mammiferi') {
+        animaliMammiferi.push(animale)
+    } 
+}
+console.log(animaliMammiferi);
+
+   
